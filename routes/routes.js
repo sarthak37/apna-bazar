41a9ff4897cb335ca12ab70c7413a793d66b691e
sarthak37 <sarthak27998@gmail.com>
2021-09-26 14:47:38 +0530

    update

--- routes/routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { userSignUp, userLogIn } from '../controller/user-controller.js';
-
-const router = express.Router();
-router.post('/signup', userSignUp);
-router.post('/login', userLogIn);
-
-export default router;
